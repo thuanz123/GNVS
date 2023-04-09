e12529798c90345b02079bb4bfd8a23e22e87b2c
ItsThanhTung <tung.10hoa.2016@gmail.com>
2023-04-09 16:26:47 +0700

    basic structure

---
+++ eval.py
@@ -0,0 +1,13 @@
+# load config
+# load model
+# load daset
+
+
+# start testing loop
+
+    # calculate score
+
+    # save result
+
+
+# calculate average score
---
+++ train.py
@@ -0,0 +1,18 @@
+# load config   # from config parser
+# load model    # load model + renderer
+# load daset    # 
+
+
+# start training loop
+
+    # after some iteration 
+    # evaluate validation dataset
+    # visualize 
+
+
+    # after some iteration
+    # save checkpoint
+
+
+    # logging loss
+
